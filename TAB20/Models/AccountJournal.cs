e24546e37439af8489e68a7f584696759ac9a5fc
okita <t-okita@netoffice.obisan.co.jp>
2023-10-30 09:41:26 +0900

    AccountJournal.cs

---
+++ TAB20/Models/AccountJournal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAB20.Models
+{
+    public class AccountJournal
+    {
+        public int Id { get; set; }
+
+        [Column("journal_date")]
+        public DateTime JournalDate { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("debit_account_id")]
+        public int DebitAccountId { get; set; }
+
+        [Column("debit_account_name")]
+        public string DebitAccountName { get; set; }
+
+        [Column("credit_account_id")]
+        public int CreditAccountId { get; set; }
+
+        [Column("credit_account_name")]
+        public string CreditAccountName { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+    }
+}
